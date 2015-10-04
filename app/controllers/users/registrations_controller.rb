b97ad3a33f9cd9bf677fd631c922be7d03d7ab87
Asfa Lohani <asfa.rani@gmail.com>
2015-10-04 02:33:30 +0000

    Fixed pro form and stripe error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,11 +5,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
        super do |resource|
         if params[:plan]
             resource.plan_id = params[:plan]
-            if resource.plan_id == 2
-                resource.save_with_payment
-            else
-                resource.save
-            end
+                if resource.plan_id == 2
+                    resource.save_with_payment
+                else
+                    resource.save
+                end
         end
        end
     end
